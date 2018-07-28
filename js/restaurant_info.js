e8522337185dd9350ae4d6554e54aa3ffd8d9020
Monica Moura <monica.a.moura@hotmail.com>
2018-07-28 11:43:21 +0100

    make layout responsive

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoibmVnbWFoIiwiYSI6ImNqazMwa3RvbDB2YWEzcHM0bXlhem9meGoifQ.VXL7B7WlMDpmvzCER_HQPQ',
+        mapboxToken: 'pk.eyJ1IjoibmVnbWFoIiwiYSI6ImNqazMwa3RvbDB2YWEzcHM0bXlhem9meGoifQ.VXL7B7WlMDpmvzCER_HQPQ', 
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
