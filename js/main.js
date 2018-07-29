597564380160358cb0cbbb4dda4fe6699860a5ed
Monica Moura <monica.a.moura@hotmail.com>
2018-07-29 14:56:54 +0100

    service worker

--- js/main.js
+++ js/main.js
@@ -212,3 +212,23 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
+/*** ADD SERVICE WORKER ***/
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(reg => {
+      //registration is successful
+      if (reg.installing) {
+        console.log("Service worker installing");
+      } else if (reg.waiting) {
+        console.log("Service worker installed");
+      } else if (reg.active) {
+        console.log("Service worker active");
+      }
+      console.log("Registration succeeded. Scope is " + reg.scope);
+    })
+    .catch(error => {
+      //registration has failed
+      console.log("Registration failed at " + error);
+    });
+}
