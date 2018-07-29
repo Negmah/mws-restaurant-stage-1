b93fe5fca35f17c0974be47fb87aad67155104a0
Monica Moura <monica.a.moura@hotmail.com>
2018-07-29 16:06:51 +0100

    add eventListenter on activate

--- service-worker.js
+++ service-worker.js
@@ -31,7 +31,21 @@ self.addEventListener('install', function(event) {
     );
 });
 
-self.addEventListener('fetsch', function (event) {
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {
             return response || fetch(event.request);
